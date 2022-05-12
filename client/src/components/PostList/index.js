eb2d4b8960edb5ac2f37756ddad60d20c4128896
Samer Balee <samerbalee@gmail.com>
2022-05-12 20:41:15 +1000

    make it better2

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -15,11 +15,11 @@ const PostList = ({ posts }) => {
 
       {posts &&
         posts.map((post) => (
-          <div key={post._id} className='block p-6 rounded-lg shadow-lg bg-white max-w-2xl my-10'>
-            <div className='flex flex-row'>
+          <div key={post._id} className='block p-6 rounded-lg shadow-lg bg-gray-100 max-w-2xl my-10'>
+            <div className='flex flex-row bg-green-100'>
               <img
                 src=""
-                className="rounded-full w-32"
+                className="rounded-full w-32 "
                 alt="Avatar"
               />
               <h4 >{post.postAuthor}</h4>
@@ -29,14 +29,24 @@ const PostList = ({ posts }) => {
                  <h4 >{post.createdAt}</h4>
             </div>
             <br />
-            <h5 className="p-2">
+            <h5 className="font-bold">
               {post.title}
             </h5>
-            <div className="p-4">
+            <div className="bg-white">
               <p>{post.text}</p>
             </div>
+            <div className='flex justify-between'>
+              <button className='mt-4 text-blue-500'>Like {post.likeCount}</button>
+              <button className="
+                  bg-green-200 rounded-md
+                  w- mt-3 h-8 block
+                  hover:bg-green-300 font-medium"
+            type="submit"
+          >Delete post
+          </button>
+            </div>
             
-           <FontAwesomeIcon icon="fa-regular fa-thumbs-up" />
+           {/* <FontAwesomeIcon icon="fa-regular fa-thumbs-up" /> */}
 
             
           </div>
